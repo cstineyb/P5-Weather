a3ef0303ab10337c84dfafd29240951f26e564dd
Stine Hansen <stinehansen@Stines-MacBook-Pro.local>
2015-03-02 17:08:21 -0800

    add webfont icons

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 
 
-// Docs at http://simpleweatherjs.com
+/*// Docs at http://simpleweatherjs.com
 $(document).ready(function() {
   $.simpleWeather({
     location: 'Spokane, WA',
@@ -18,4 +18,48 @@ $(document).ready(function() {
       $("#weather").html('<p>'+error+'</p>');
     }
   });
-});
+});*/
+
+
+$.simpleWeather({
+
+    location: 'Spokane, WA',
+    woeid: '12799529',
+    unit: 'f',
+    
+    // Get _weather_ object
+    success: function(weather) {
+      
+      // Get & store temperature
+      var temp = weather.temp;
+      // Get & store city
+      var city = weather.city;
+       // Get & store state
+      var state = weather.region;
+      // Get & store thumb
+      var thumb = weather.thumbnail;
+      // Get & store relative humidity
+
+      
+      console.log(thumb)
+      
+      // Output to hooks in HTML
+      $('.temp').text(temp + "F");
+      $('.city').text(city);
+      //OUTPUT STATE
+      $('.state').text(state);
+      
+      //get and store thumbnail
+       $('.thumb img').attr('src', thumb);
+      
+      
+      // See console for _weather_ object
+      console.log(weather);
+    },
+  
+    // if error
+    error: function(error) {  
+      $('body').html('<p>' + error + '</p>');
+    }
+  
+  });
